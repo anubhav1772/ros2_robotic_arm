23dfeb39076c58bb004fa6c9ab204e251beaeb0e
anubhav1772 <anubhavsingh55182@gmail.com>
2023-07-18 20:07:14 +0530

    unedited; package restructuring

---
+++ ur5_robotic_arm/src/ur5_gazebo/launch/test.launch.py
@@ -0,0 +1,49 @@
+"""Launch Gazebo server and client with command line arguments."""
+"""Spawn robot from URDF file."""
+
+import os
+from launch import LaunchDescription
+from launch.actions import TimerAction, DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, PathJoinSubstitution, FindExecutable, LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import xacro
+from launch_ros.parameter_descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')   
+
+    gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),)
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    robot_name = 'ur'
+    world_file_name = 'empty.world'
+
+    world = os.path.join(get_package_share_directory('ur5_gazebo'), 'worlds', world_file_name)
+
+    urdf = os.path.join(get_package_share_directory('ur5_description'), 'urdf', 'ur.urdf.xacro')
+
+    xml = open(urdf, 'r').read()
+
+    xml = xml.replace('"', '\\"')
+
+    swpan_args = '{name: \"my_robot\", xml: \"' + xml + '\" }'
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', world,
+                 '-s', 'libgazebo_ros_factory.so'],
+            output='screen'),
+
+        ExecuteProcess(
+            cmd=['ros2', 'param', 'set', '/gazebo',
+                 'use_sim_time', use_sim_time],
+            output='screen'),
+
+        ExecuteProcess(
+            cmd=['ros2', 'service', 'call', '/spawn_entity',
+                 'gazebo_msgs/SpawnEntity', swpan_args],
+            output='screen'),
+    ])
